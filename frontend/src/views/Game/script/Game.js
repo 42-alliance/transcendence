a8f4e93c290974308e40dd6dcbc1a6b37d0fea2a
Mohamed Dembele <mdembele@bess-f2r6s5.clusters.42paris.fr>
2025-02-25 20:52:00 +0100

    add canva

--- frontend/src/views/Game/script/Game.js
+++ frontend/src/views/Game/script/Game.js
@@ -1,17 +1,30 @@
-var username = 'Test';
-function render() {
-    var socket = new WebSocket("ws://localhost:8790");
-    if (!socket) {
-        console.error('Impossible de se connecter au serveur WebSocket');
-        return;
+var username = 'Test'; // À remplacer par un fetch
+var canvas = document.getElementById("game-canvas");
+function drawing() {
+    var ctx = canvas.getContext("2d");
+    if (canvas.getContext) {
+        // Create an Image object to load a GIF
+        var gifImage_1 = new Image();
+        gifImage_1.src = './asset/waiting.gif'; // Replace with the path to your GIF file
+        // Draw the GIF on the canvas once it has loaded
+        gifImage_1.onload = function () {
+            // You can set the position and size of the GIF as needed
+            ctx.drawImage(gifImage_1, 0, -100, 900, 900); // x, y, width, height
+        };
     }
+}
+function render() {
+    connectWebSocket();
+    drawing();
+}
+function connectWebSocket() {
+    var socket = new WebSocket("ws://localhost:8790");
     socket.onopen = function () {
-        console.log('Connecté au serveur WebSocket');
+        //console.log('Connecté au serveur WebSocket');
         socket.send(JSON.stringify({ type: 'online', username: username }));
-        console.log('Message envoyé');
     };
-    socket.onmessage = function (event) {
-        console.log(event.data);
+    socket.onerror = function (error) {
+        console.error('Erreur WebSocket:', error);
     };
 }
 window.render = render;
--- frontend/src/views/Game/script/Game.ts
+++ frontend/src/views/Game/script/Game.ts
@@ -1,17 +1,38 @@
-//TODO: fetch to get real username and id
-let username = 'Test';
-//END
+let username = 'Test'; // À remplacer par un fetch
+
+const canvas = <HTMLCanvasElement> document.getElementById("game-canvas");
+
+function drawing() {
+	const ctx = canvas.getContext("2d");
+	if (canvas.getContext) {
+		// Create an Image object to load a GIF
+		const gifImage = new Image();
+		gifImage.src = './asset/waiting.gif'; // Replace with the path to your GIF file
+
+		// Draw the GIF on the canvas once it has loaded
+		gifImage.onload = () => {
+			// You can set the position and size of the GIF as needed
+			ctx.drawImage(gifImage, 0, -100, 1000, 900); // x, y, width, height
+ 		
+		};
+	}
+}
 
 function render() {
+	connectWebSocket();
+	drawing();
+}
+
+function connectWebSocket() {
 	const socket = new WebSocket("ws://localhost:8790");
-	if (!socket) {
-		console.error('Impossible de se connecter au serveur WebSocket');
-		return;
-	}
+
 	socket.onopen = () => {
-		console.log('Connecté au serveur WebSocket');
+		//console.log('Connecté au serveur WebSocket');
 		socket.send(JSON.stringify({ type: 'online', username: username }));
-		console.log('Message envoyé');
+	};
+
+	socket.onerror = (error) => {
+		console.error('Erreur WebSocket:', error);
 	};
 }
 
