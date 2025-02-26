729dd26d1301fc338a7e75ada1c44729775230fd
JudaNana <moussaadamou898@gmail.com>
2025-02-26 16:28:26 +0100

    rewrite script.ts

--- frontend/script.ts
+++ frontend/script.ts
@@ -1,12 +1,4 @@
-/**
- * Fetches data from the given API URL with the specified options.
- * 
- * @param url - The URL to fetch data from.
- * @param options - The options to use for the fetch request.
- * @returns The response from the fetch request.
- * @throws Will throw an error if the fetch request fails.
- */
-export async function fetchApi(url: string, options: RequestInit): Promise<Response>;
+
 
 /**
  * Checks if the user is logged in by calling the `me` function.
@@ -20,14 +12,14 @@ export async function userIsLogin(): Promise<boolean>;
  * 
  * @param user - The user data to store in session storage.
  */
-function setSessionStorage(user: IUserData): void;
+function setSessionStorage(user: IUser): void;
 
 /**
  * Fetches the current user's data from the server.
  * 
  * @returns The user data if the request is successful, otherwise null.
  */
-export async function me(): Promise<IUserData | null>;
+export async function me(): Promise<IUser | null>;
 
 /**
  * Logs in the user with the given username.
@@ -75,10 +67,17 @@ export async function addUserApi(username: string, profilePicture: File): Promis
 async function getAuthUrl(): Promise<void>;
 import { cleanSessionStorage } from "./src/views/navbar/navbar.js";
 import { navigateTo } from "./src/views/viewManager.js";
-import { IUserData } from "./types.js";
+import { IUser } from "./types.js";
 
-
-export async function fetchApi(url: string, options: RequestInit) {
+/**
+ * Fetches data from the given API URL with the specified options.
+ * 
+ * @param url - The URL to fetch data from.
+ * @param options - The options to use for the fetch request.
+ * @returns The response from the fetch request.
+ * @throws Will throw an error if the fetch request fails.
+ */
+export async function fetchApi(url: string, options: RequestInit): Promise<Response> {
 
 	const response = await fetch(url, options);
 
@@ -97,7 +96,7 @@ export async function userIsLogin() {
 	return false;
 }
 
-function setSessionStorage(user: IUserData) {
+function setSessionStorage(user: IUser) {
 	sessionStorage.setItem("userId", user.id.toString());
 	sessionStorage.setItem("userPicture", user.picture);
 	sessionStorage.setItem("username", user.name);
@@ -146,25 +145,18 @@ export async function logUserApi(username: string) {
 async function deleteUserImage() {
     const user = await me();
 
-    if (user === null || user.upload_picture === null) {
+    if (user === null || user.picture === null) {
         console.error("User or user image not found");
         return;
     }
-
     try {
         const headers = new Headers();
-        const pathDelete = user.upload_picture;
+        const pathDelete = user.picture;
 
-        const response = await fetch(pathDelete, {
-            method: "DELETE",
-            headers: headers,
-        });
-
-        if (!response.ok) {
-            console.error("Failed to delete user picture:", await response.text());
-            return;
-        }
-
+		await fetchApi(pathDelete, {
+			method: "DELETE",
+			headers: headers,
+		});
         console.log("User picture successfully deleted!");
     } catch (error) {
         console.error("Internal server error:", error);
@@ -177,15 +169,11 @@ export async function deleteUserApi() {
 		headers.append('Content-Type', 'application/json');
 		
 		await deleteUserImage();
-		const response = await fetch('http://localhost:8000/api/me/', {
+		await fetchApi('http://localhost:8000/api/me/', {
 			method: 'DELETE',
 			headers: headers,
 			credentials: 'include', // send cookies
-		})
-		if (!response.ok) {
-			console.error("Failed to logout user")
-			return false;
-		}
+		});
 		cleanSessionStorage();
 		console.log("User  succesfuly!");
 		navigateTo("/auth");
@@ -195,20 +183,15 @@ export async function deleteUserApi() {
 }
 
 export async function logOutUserApi() {
-	console.log("Tentative de deconnexion de l'utilisateur")
 	try {
 		const headers = new Headers();
 		headers.append('Content-Type', 'application/json');
 		
-		const response = await fetch('http://localhost:8000/api/logout/', {
+		await fetchApi('http://localhost:8000/api/logout/', {
 			method: 'GET',
 			headers: headers,
-			credentials: 'include', // send cookies
-		})
-		if (!response.ok) {
-			console.error("Failed to logout user")
-			return false;
-		}
+			credentials: 'include',
+		});
 		cleanSessionStorage();
 		console.log("User diconected succesfuly!");
 		navigateTo("/auth");
@@ -224,18 +207,12 @@ async function uploadProfilePicture(username: string, profilePicture: File) : Pr
 
         const pathUpload = `http://localhost:9000/upload/${username}_${profilePicture.name}`;
         const pathDownload = `http://localhost:9000/files/${username}_${profilePicture.name}`;
-
-        const response = await fetch(pathUpload, {
+// TODO: faire en sorte que l'api retourne le path de l'image
+        await fetchApi(pathUpload, {
             method: "PUT",
             headers: headers,
             body: profilePicture,
         });
-
-        if (!response.ok) {
-            console.error("File upload failed:", await response.text());
-            return null;
-        }
-
         return pathDownload;
     } catch (error) {
         console.error("Error uploading file:", error);
@@ -244,32 +221,24 @@ async function uploadProfilePicture(username: string, profilePicture: File) : Pr
 }
 
 export async function addUserApi(username: string, profilePicture: File) : Promise<void> {
-	console.log("Tentative de creation du nouvel utilisateur en cour ...")
 	try {
 		const headers = new Headers();
 		headers.append('Content-Type', 'application/json');
 		
-        let filePath: string | null;
-        if (profilePicture) {
-            filePath = await uploadProfilePicture(username, profilePicture);
-            if (!filePath) {
-                console.error("File upload failed. User creation aborted.");
-                return;
-            }
-        }
-		const response = await fetch('http://localhost:8000/api/user/', {
+        let filePath = await uploadProfilePicture(username, profilePicture);
+		if (!filePath) {
+			console.error("File upload failed. User creation aborted.");
+			return;
+		}
+		await fetchApi('http://localhost:8000/api/user/', {
 			method: 'POST',
 			headers: headers,
-			credentials: 'include', // send cookies
+			credentials: 'include',
 			body: JSON.stringify({
 				'username': username,
 				'upload_picture': filePath,
 			})
-		})
-		if (!response.ok) {
-			console.error("failed to check user creation")
-			return;
-		}
+		});
 		console.log("User creation test succesed");
 		navigateTo("/");
 	} catch (e) {
@@ -281,15 +250,10 @@ async function getAuthUrl() {
 	try {
 		const headers = new Headers();
 
-		const response = await fetch('http://localhost:8000/auth/redirect/', {
+		const response = await fetchApi('http://localhost:8000/auth/redirect/', {
 			method: 'GET',
 			headers: headers,
 		});
-
-		if (!response.ok) {
-			console.error("failed to get auth url")
-			return;
-		}
 		const data = await response.json();
 		if (data.auth_url) {
 			console.log(data.auth_url)
@@ -302,8 +266,7 @@ async function getAuthUrl() {
 	}
 }
 
-
 // Ajoutez la fonction au contexte global
-window.getAuthUrl = getAuthUrl;
-window.logOutUserApi = logOutUserApi;
-window.deleteUserApi = deleteUserApi;
+(window as any).getAuthUrl = getAuthUrl;
+(window as any).logOutUserApi = logOutUserApi;
+(window as any).deleteUserApi = deleteUserApi;
--- frontend/types.ts
+++ frontend/types.ts
@@ -1,5 +1,5 @@
 
-export interface IUserData {
+export interface IUser {
 	id: number;
 	name: string;
 	picture: string;
