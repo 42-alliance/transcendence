f01431e78d14707120945f1626ce07255bf006f5
pepedinho <2spii94@gmail.com>
2025-02-26 17:56:44 +0100

    added current friends relation in /users/@me route

--- services/user/src/users/@me/route.ts
+++ services/user/src/users/@me/route.ts
@@ -8,12 +8,56 @@ export async function me(server:FastifyInstance, request: FastifyRequest, reply:
         const me = await prisma.users.findUnique({
             where: {
                 id: id,
+            },
+            include: {
+                sentRequests: {
+                    include: {
+                        receiver: true
+                    }
+                },
+                receivedRequests: {
+                    include: {
+                        sender: true
+                    }
+                }
             }
         });
         if (!me) {
             return reply.status(400).send({error: "User not found"});
         }
-        return reply.status(200).send({user: me});
+
+        const friendsList = [
+            ...me.sentRequests
+            .filter(friend => friend.receiver.id !== id)
+            .map(friend => ({
+                id: friend.receiver.id,
+                name: friend.receiver.name,
+                picture: friend.receiver.picture,
+                relation_id: friend.id,
+                status: friend.status,
+                type: "sent"  
+            })),
+            ...me.receivedRequests
+            .filter(friend => friend.sender.id !== id)
+            .map(friend => ({
+                id: friend.sender.id,
+                name: friend.sender.name,
+                picture: friend.sender.picture,
+                relation_id: friend.id,
+                status: friend.status,
+                type: "received"
+            }))
+        ];
+
+        return reply.status(200).send({
+            user: {
+                id: me.id,
+                name: me.name,
+                picture: me.picture,
+                created_at: me.created_at,
+                friends: friendsList
+            }
+        });
     } catch (e) {
         return reply.status(500).send({error: e});
     }
