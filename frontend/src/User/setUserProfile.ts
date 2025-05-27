6601e5e11c0df80d45be2b24715c54bf1ad2d1e3
JudaNanaa <moussaadamou898@gmail.com>
2025-05-27 18:04:37 +0200

    start change dashboard

--- frontend/src/User/setUserProfile.ts
+++ frontend/src/User/setUserProfile.ts
@@ -1,28 +1,40 @@
 import { deleteUser } from "./deleteUser.js";
 import { getUserInfos } from "./me.js";
 
-
 export async function setUserProfile() {
-	const profileButton = document.getElementById("userProfile-button");
-	if (!profileButton) return;
+	const userNameSpan = document.getElementById("username-navbar");
+	if (!userNameSpan) return;
+
+	const profilePicture = document.getElementById(
+		"profile-picture-navbar"
+	) as HTMLImageElement;
+	if (!profilePicture) return;
 
 	const user = await getUserInfos();
 	if (!user || !user.name || !user.picture) return;
 
-	profileButton.innerHTML = "";
+	userNameSpan.innerText = user.name;
+	profilePicture.src = user.picture;
 
-	const userImg = document.createElement('img');
-	userImg.classList.add("rounded-[50%]", "w-10", "h-10");
-	userImg.src = user.picture;
-	userImg.alt = `${user.name} picture`;
+	const usernameDropdown = document.getElementById("username-dropdown");
+	if (!usernameDropdown) return;
 
-	const userSpan = document.createElement('span');
-	userSpan.innerText = user.name;
+	usernameDropdown.innerText = user.name;
 
-	profileButton.onclick = async () => {
-		await deleteUser();
-	}
+	const userButton = document.getElementById("user-button-navbar");
+	if (!userButton) return;
 
-	profileButton.appendChild(userImg);
-	profileButton.appendChild(userSpan);
+	userButton.onclick = async () => {
+		const dropDown = document.getElementById("dropdown-user");
+		if (!dropDown) return;
+
+		if (userButton.ariaExpanded === "false") {
+			dropDown.classList.remove("hidden");
+
+			userButton.ariaExpanded = "true";
+		} else {
+			dropDown.classList.add("hidden");
+			userButton.ariaExpanded = "false";
+		}
+	};
 }
